03f2a05871786ace624a08d3ca2311970d2a754f
rodrigomha <rodrigomha@gmail.com>
2025-03-12 16:48:13 -0700

    update add variable to common

--- src/technology_models/technologies/common/add_variable.jl
+++ src/technology_models/technologies/common/add_variable.jl
@@ -42,6 +42,49 @@ function add_variable!(
     container::SingleOptimizationContainer,
     variable_type::T,
     devices::U,
+    formulation::V,
+) where {
+    T <: InvestmentVariableType,
+    U <: Vector{D},
+    V <: IntegerInvestment,
+} where {D <: PSIP.Technology}
+    @assert !isempty(devices)
+    time_mapping = get_time_mapping(container)
+    time_steps = get_investment_time_steps(time_mapping)
+    tech_model = string(V)
+
+    names = [PSIP.get_name(d) for d in devices]
+    check_duplicate_names(names, container, variable_type, D)
+
+    variable = add_variable_container!(
+        container,
+        variable_type,
+        D,
+        names,
+        time_steps,
+        meta=tech_model,
+    )
+    for t in time_steps, d in devices
+        name = PSY.get_name(d)
+        variable[name, t] = JuMP.@variable(
+            get_jump_model(container),
+            base_name = "$(T)_$(D)_{$(name), $(t)}",
+            integer = true,
+        )
+        ub = get_variable_upper_bound(variable_type, d, formulation)
+        ub !== nothing && JuMP.set_upper_bound(variable[name, t], ub)
+
+        lb = get_variable_lower_bound(variable_type, d, formulation)
+        lb !== nothing && JuMP.set_lower_bound(variable[name, t], lb)
+    end
+
+    return
+end
+
+function add_variable!(
+    container::SingleOptimizationContainer,
+    variable_type::T,
+    devices::U,
     formulation::S,
 ) where {
     T <: OperationsVariableType,
