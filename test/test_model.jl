73e87815cd6be6a22d270c64607e7a133c241ccb
rodrigomha <rodrigomha@gmail.com>
2025-03-13 10:28:42 -0700

    update test to use storage

--- test/test_model.jl
+++ test/test_model.jl
@@ -59,6 +59,15 @@
         BasicDispatchFeasibility,
     )
 
+    set_technology_model!(
+        template,
+        ["test_storage"],
+        PSIP.StorageTechnology{EnergyReservoirStorage},
+        ContinuousInvestment,
+        CyclicalStorageDispatch,
+        BasicDispatchFeasibility,
+    )
+
     m = InvestmentModel(
         template,
         SingleInstanceSolve,
@@ -73,7 +82,77 @@
     @test solve!(m) == PSIN.RunStatus.SUCCESSFULLY_FINALIZED
 
     res = OptimizationProblemResults(m)
-    @test length(IS.Optimization.list_variable_names(res)) == 6
+    @test length(IS.Optimization.list_variable_names(res)) == 11
+    @test length(IS.Optimization.list_dual_names(res)) == 0
+    @test length(PSIN.get_timestamps(res)) == 48
+
+    template = InvestmentModelTemplate(
+        capital,
+        operations,
+        feasibility,
+        TransportModel(MultiRegionBalanceModel, use_slacks=false),
+    )
+
+    set_technology_model!(
+        template,
+        ["demand1", "demand2"],
+        PSIP.DemandRequirement{PSY.PowerLoad},
+        StaticLoadInvestment,
+        BasicDispatch,
+        BasicDispatchFeasibility,
+    )
+
+    set_technology_model!(
+        template,
+        ["wind"],
+        PSIP.SupplyTechnology{PSY.RenewableDispatch},
+        ContinuousInvestment,
+        BasicDispatch,
+        BasicDispatchFeasibility,
+    )
+
+    set_technology_model!(
+        template,
+        ["cheap_thermal", "expensive_thermal"],
+        PSIP.SupplyTechnology{PSY.ThermalStandard},
+        ContinuousInvestment,
+        BasicDispatch,
+        BasicDispatchFeasibility,
+    )
+
+    set_technology_model!(
+        template,
+        ["test_branch"],
+        PSIP.ACTransportTechnology{PSY.ACBranch},
+        ContinuousInvestment,
+        BasicDispatch,
+        BasicDispatchFeasibility,
+    )
+
+    set_technology_model!(
+        template,
+        ["test_storage"],
+        PSIP.StorageTechnology{EnergyReservoirStorage},
+        ContinuousInvestment,
+        ChronologicalStorageDispatch,
+        BasicDispatchFeasibility,
+    )
+
+    m = InvestmentModel(
+        template,
+        SingleInstanceSolve,
+        p_5bus;
+        optimizer=HiGHS.Optimizer,
+        portfolio_to_file=false,
+        store_variable_names=true,
+    )
+
+    @test build!(m; output_dir=mktempdir(; cleanup=true)) ==
+          IS.Optimization.ModelBuildStatusModule.ModelBuildStatus.BUILT
+    @test solve!(m) == PSIN.RunStatus.SUCCESSFULLY_FINALIZED
+
+    res = OptimizationProblemResults(m)
+    @test length(IS.Optimization.list_variable_names(res)) == 11
     @test length(IS.Optimization.list_dual_names(res)) == 0
     @test length(PSIN.get_timestamps(res)) == 48
 end
