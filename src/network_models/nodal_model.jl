f85526e8d75b926d9f629329b88d37ba15c9207a
Barrows <clayton.barrows@nrel.gov>
2026-09-14 11:23:41 -0400

    Add power balance slack variables with penalty cost
    - NodalBalanceConstraint now uses slack variables: expressions[n,t] == slack[n,t]
    - Slack penalty: 1 million per MWh (very high to signal infeasibility)
    - Allows infeasible scenarios to solve with nonzero slacks indicating the problem
    - Slacks are added to objective function with penalty cost

--- src/network_models/nodal_model.jl
+++ src/network_models/nodal_model.jl
@@ -1,5 +1,8 @@
 # Nodal power balance model: energy balance constraint at each bus (node).
 # Structure mirrors multiregion_model.jl but uses PSIP.Node instead of PSIP.Zone.
+# Now includes slack variables for infeasible scenarios.
+
+const NODAL_BALANCE_SLACK_PENALTY = 1_000_000.0  # $1M/MWh penalty for power balance violations
 
 function add_constraints!(
     container::SingleOptimizationContainer,
@@ -11,10 +14,22 @@ function add_constraints!(
     nodes = PSIP.get_name.(PSIP.get_regions(PSIP.Node, port))
     expressions = get_expression(container, EnergyBalance(), U)
     constraint = add_constraints_container!(container, T(), U, nodes, time_steps)
+
+    # Create slack variables for power balance violations
+    jm = get_jump_model(container)
+    slack_vars = @variable(jm, nodal_balance_slack[n in nodes, t in time_steps] >= 0)
+
+    # Add constraints with slack: expressions[n, t] == slack[n, t]
+    # (slack allows energy imbalance when generation cannot meet demand)
     for t in time_steps, n in nodes
         constraint[n, t] =
-            JuMP.@constraint(get_jump_model(container), expressions[n, t] == 0)
+            JuMP.@constraint(jm, expressions[n, t] == slack_vars[n, t])
     end
+
+    # Add slack penalty to objective
+    current_obj = JuMP.objective_function(jm)
+    penalty_cost = sum(slack_vars) * NODAL_BALANCE_SLACK_PENALTY
+    JuMP.set_objective(jm, JuMP.MOI.MIN_SENSE, current_obj + penalty_cost)
 
     return
 end
